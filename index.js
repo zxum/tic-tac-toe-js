ea8fe11eb9adffb6c79edf72e98905a60a0a816e
zxum <zion.um@outlook.com>
2021-02-19 15:45:53 -0600

    Add AI functionality

--- index.js
+++ index.js
@@ -4,15 +4,35 @@ const menu = (function() {
     let menuContainer = document.getElementById('menu-modal');
     let playerButton = document.getElementById('player');
     let aiButton = document.getElementById('ai');
+    let playerModal = document.getElementById("player-modal")
+    let aiModal = document.getElementById("ai-modal")
+
+    let menuButton = document.getElementById('menu-btn')
 
     //bind events
-    playerButton.addEventListener('click', closeModal)
-    aiButton.addEventListener('click', closeModal)
+    playerButton.addEventListener('click', selectPlayerModal)
+    aiButton.addEventListener('click', selectAIModal)
+    menuButton.addEventListener('click', openModal)
 
-    function closeModal() {
+    function selectPlayerModal() {
         menuContainer.style.display = "none";
+        aiModal.style.display = "none";
     }
 
+    function selectAIModal() {
+        menuContainer.style.display = "none";
+        playerModal.style.display = "none";
+    }
+
+    function openModal() {
+        window.location.reload(true)
+            // menuContainer.style.display = "block";
+            // playerModal.style.display = "block";
+            // aiModal.style.display = "block";
+            // gameBoard.clearBoard();
+            // console.log(`After I clicked menu, the turn is ${turn}`)
+            // console.log(`After I click the menu, positions are ${positions}`)
+    }
 
 })();
 
@@ -24,12 +44,14 @@ const createPlayer = (playerName, playerToken) => {
     }
 };
 
-let player1 = createPlayer("", "X")
-let player2 = createPlayer("", "O")
+
 
 // PLAYER INPUT MODULE
 
 const playerInput = (function() {
+    let player1 = createPlayer("", "X")
+    let player2 = createPlayer("", "O")
+
     //cache DOM
     let playerModal = document.getElementById('player-modal')
     let playerSubmitButton = document.getElementById('player-submit')
@@ -41,22 +63,61 @@ const playerInput = (function() {
     playerSubmitButton.addEventListener('click', addToPlayerBox)
 
     function addToPlayerBox() {
-        // var player1obj = createPlayer(document.getElementById('player1-nameField').value, 'X')
-        // var player2obj = createPlayer(document.getElementById('player2-nameField').value, 'O')
         player1.playerName = document.getElementById('player1-nameField').value
         player2.playerName = document.getElementById('player2-nameField').value
-        player1Box.innerText = player1.playerName
-        player2Box.innerText = player2.playerName
-        document.forms[0].reset();
-        playerModal.style.display = "none";
 
-    }
+        if ((player1.playerName == "") || (player2.playerName == "")) {
+            alert("Please enter a valid name!")
+            return addToPlayerBox()
+        } else {
+            player1Box.innerText = player1.playerName
+            player2Box.innerText = player2.playerName
+            document.forms[0].reset();
+            playerModal.style.display = "none";
+            gameBoard.playPlayerGame()
+        }
+    };
 
     return {
 
     }
 })();
 
+// AI INPUT MODULE 
+
+const aiInput = (function() {
+    let player1 = createPlayer("", "X")
+    let player2 = createPlayer("Computer", "O")
+
+    //cache DOM 
+    let aiModal = document.getElementById('ai-modal')
+    let aiSubmitButton = document.getElementById('ai-submit')
+    let player1Box = document.getElementById('player1name')
+    let player2Box = document.getElementById('player2name')
+
+    //bind events 
+    aiSubmitButton.addEventListener('click', addToPlayerBox)
+
+    // functions 
+    function addToPlayerBox() {
+        player1.playerName = document.getElementById('user-nameField').value
+
+        if (player1.playerName == "") {
+            alert("Please enter a valid name!")
+            return addtoPlayerBox()
+        } else {
+            player1Box.innerText = player1.playerName
+            player2Box.innerText = player2.playerName
+            document.forms[1].reset();
+            aiModal.style.display = "none";
+            gameBoard.playAIGame()
+        }
+    }
+
+    return {
+
+    }
+})();
 
 // GAMEBOARD MODULE
 
@@ -75,29 +136,135 @@ const gameBoard = (function() {
     ]
 
     //cache DOM 
-    let gameboard = document.getElementById("gameboard")
+
     let tiles = Array.from(document.getElementsByClassName('tile'))
+    let restartButton = document.getElementById("restart-btn")
+    let player1Box = document.getElementById("player1")
+    let player2Box = document.getElementById("player2")
+
 
     //bind events 
-    tiles.forEach(item => {
-        item.addEventListener('click', e => {
-            if (turn % 2 == 0) {
-                addO(e)
-            } else {
-                addX(e)
-            }
-            turn++
-            checkWinner()
-            checkTurn()
-            console.log(positions)
-            console.log(turn)
+    restartButton.addEventListener('click', clearBoard);
 
-        })
-    })
+
+
 
     //functions
+    function playPlayerGame() {
+
+        tiles.forEach(item => {
+            item.addEventListener('click', e => {
+                if (turn % 2 == 0) {
+                    addO(e)
+                } else {
+                    addX(e)
+                }
+                turn++
+                showTurn()
+                checkWinner()
+                checkTie()
+            })
+        })
+    }
+
+    function playAIGame() {
+        tiles.forEach(item => {
+            item.addEventListener('click', e => {
+                if (turn % 2 != 0) {
+                    console.log(`the turn is ${turn}`)
+                        // Player Turn 
+                    addX(e)
+                    turn++
+                    showTurn()
+                    checkWinner()
+                    checkTie()
+
+                    // AI Turn 
+                    aiTurn()
+                    turn++
+                    showTurn()
+                    checkWinner()
+                    checkTie()
+                }
+            })
+        });
+    }
+
+    function showTurn() {
+        if (turn % 2 == 0) {
+            player1Box.classList.remove("highlight")
+            player2Box.classList.add("highlight")
+        } else {
+            player2Box.classList.remove("highlight")
+            player1Box.classList.add("highlight")
+        }
+    }
+    showTurn()
+
+
+    function checkLines() {
+        let comboarray = []
+        let linearray = []
+
+        winning_array.forEach(combo => {
+            let pos = {}
+            pos[combo[0]] = positions[combo[0]]
+            pos[combo[1]] = positions[combo[1]]
+            pos[combo[2]] = positions[combo[2]]
+
+            let parr = Object.values(pos)
+            let line = parr.sort().join("")
+
+            comboarray.push(pos)
+            linearray.push(line)
+        })
+
+        if (linearray.includes("XX")) {
+            let index = linearray.indexOf("XX")
+            let c = comboarray[index]
+            return getKeyByValue(c, "")
+        } else if (linearray.includes("OO")) {
+            let index = linearray.indexOf("OO")
+            let c = comboarray[index]
+            return getKeyByValue(c, "")
+        } else {
+            console.log(`The index of an empty string is ${positions.indexOf("")}`)
+            return positions.indexOf("")
+        }
+    }
+
+    function getKeyByValue(object, value) {
+        return Object.keys(object).find(key => object[key] === value);
+    }
+
+    function selectTile(tile) {
+        console.log(`Inside the selectTile function, positions is ${positions}`)
+        let target = "tile" + tile
+        targetTile = document.getElementById(target)
+        positions[target] = "O"
+        targetTile.innerHTML = "<i class='fas fa-circle fa-4x'></i>"
+        targetTile.classList.add('nohover')
+    }
+
+
+    function aiTurn() {
+        if (turn == 2) {
+            if (positions[0] == "") {
+                selectTile(0)
+                positions[0] = "O"
+            } else {
+                selectTile(1)
+                positions[1] = "O"
+            }
+        } else {
+            var selection = checkLines()
+            selectTile(selection)
+            positions[selection] = "O"
+        }
+
+    }
+
     function addX(e) {
-        console.log(e.target)
         let index = e.target.id.slice(-1)
         positions[index] = "X"
         e.target.innerHTML = "<i class='fas fa-times fa-5x'></i>"
@@ -105,7 +272,6 @@ const gameBoard = (function() {
     }
 
     function addO(e) {
-        console.log(e.target)
         let index = e.target.id.slice(-1)
         positions[index] = "O"
         e.target.innerHTML = "<i class='fas fa-circle fa-4x'></i>"
@@ -113,33 +279,27 @@ const gameBoard = (function() {
     }
 
     function checkWinner() {
-        console.log("checking winner")
         winning_array.forEach(combo => {
-            console.log(combo)
             if (positions[combo[0]] == "X" && positions[combo[1]] == "X" && positions[combo[2]] == "X") {
-                console.log("X should win!")
                 return xwinner()
             } else if (positions[combo[0]] == "O" && positions[combo[1]] == "O" && positions[combo[2]] == "O") {
-                console.log("O should win")
                 return owinner()
             }
         });
     }
 
-    function checkTurn() {
+    function checkTie() {
         if (turn > 9) {
             return tie();
         }
     }
 
     function xwinner() {
-        console.log("X wins!")
         let player1name = document.getElementById('player1name').innerText || "X"
         alert(player1name + " wins!")
     }
 
     function owinner() {
-        console.log("O wins!")
         let player2name = document.getElementById('player2name').innerText || "O"
         alert(player2name + " wins!")
     }
@@ -148,7 +308,19 @@ const gameBoard = (function() {
         alert("It's a tie!")
     }
 
+    function clearBoard() {
+        tiles.forEach(tile => {
+            tile.innerHTML = ""
+            tile.classList.remove("nohover")
+        });
+        positions = ["", "", "", "", "", "", "", "", ""];
+        turn = 1;
+        showTurn()
+    }
+
     return {
-        positions: positions
+        clearBoard,
+        playPlayerGame,
+        playAIGame
     };
 })();
